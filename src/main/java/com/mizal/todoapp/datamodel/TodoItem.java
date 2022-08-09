2668d689f76ede0a5179f0054fedde6884f0056d
amirizal <amirizalrahmat@gmail.com>
2022-08-09 20:45:06 +0800

    override method toString in subclass todoItem to display short description in readable manner

--- src/main/java/com/mizal/todoapp/datamodel/TodoItem.java
+++ src/main/java/com/mizal/todoapp/datamodel/TodoItem.java
@@ -39,4 +39,9 @@ public class TodoItem {
     public void setDeadLine(LocalDate deadLine) {
         this.deadLine = deadLine;
     }
+
+    @Override
+    public String toString() {
+        return shortDescription;
+    }
 }
