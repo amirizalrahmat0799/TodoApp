0c666d47fd42ca09cce8e9d1447a9211953967d6
amirizal <amirizalrahmat@gmail.com>
2022-08-09 20:28:00 +0800

    adding hard coded data and function to view details

--- src/main/java/com/mizal/todoapp/Controller.java
+++ src/main/java/com/mizal/todoapp/Controller.java
@@ -1,5 +1,66 @@
 package com.mizal.todoapp;
 
+import com.mizal.todoapp.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
 
+    private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card",
+                "buy a 30th birthday card for john",
+                LocalDate.of(2022, Month.DECEMBER, 30));
+        TodoItem item2 = new TodoItem("Doctor appointment",
+                "See doctor smith at 123 elm street",
+                LocalDate.of(2022, Month.DECEMBER, 14));
+        TodoItem item3 = new TodoItem("Finish java course",
+                "to get a job eh ese",
+                LocalDate.of(2022, Month.DECEMBER, 1));
+        TodoItem item4 = new TodoItem("Have savings",
+                "for your dreams",
+                LocalDate.of(2022, Month.DECEMBER, 2));
+        TodoItem item5 = new TodoItem("maybe sleep",
+                "take care yourself from heartbreak",
+                LocalDate.of(2022, Month.DECEMBER, 31));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+//        System.out.println("The selected item is " + item);
+        StringBuilder sb = new StringBuilder();
+        sb.append(item.getDetails());
+        sb.append("\n\n\n\n");
+        sb.append("due: ");
+        sb.append(item.getDeadLine().toString());
+        itemDetailsTextArea.setText(sb.toString());
+
+
+
+    }
 }
