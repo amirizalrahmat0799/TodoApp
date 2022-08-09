b054ccc0a2111af9222d9b61dd14efa22b3a7a7c
amirizal <amirizalrahmat@gmail.com>
2022-08-09 15:12:21 +0800

    removing starter template

--- src/main/java/com/mizal/todoapp/HelloController.java
+++ src/main/java/com/mizal/todoapp/HelloController.java
@@ -1,14 +1,5 @@
 package com.mizal.todoapp;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
