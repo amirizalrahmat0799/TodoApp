5c0fed64b1a70086e9242683d2a47fac2bf59e2e
amirizal <amirizalrahmat@gmail.com>
2022-08-10 21:37:15 +0800

    Changing ui display

--- src/main/java/com/mizal/todoapp/Main.java
+++ src/main/java/com/mizal/todoapp/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 920, 540);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 340);
         stage.setTitle("Todo Application");
         stage.setScene(scene);
         stage.show();
