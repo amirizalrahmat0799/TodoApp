90037e7e48198c5856a68cd610daa9b3747cabab
amirizal <amirizalrahmat@gmail.com>
2022-08-10 22:19:23 +0800

    override method stop, init to store and load data

--- src/main/java/com/mizal/todoapp/Main.java
+++ src/main/java/com/mizal/todoapp/Main.java
@@ -1,5 +1,6 @@
 package com.mizal.todoapp;
 
+import com.mizal.todoapp.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -19,5 +20,25 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            TodoData.getInstance().storeTodoItems();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try {
+            TodoData.getInstance().loadTodoItems();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
