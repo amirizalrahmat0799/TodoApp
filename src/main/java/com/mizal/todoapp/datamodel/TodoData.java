d9a9c538b588ab8e54ff21604065b0a64ceaaa49
amirizal <amirizalrahmat@gmail.com>
2022-08-11 15:13:27 +0800

    Changing List to observableList

--- src/main/java/com/mizal/todoapp/datamodel/TodoData.java
+++ src/main/java/com/mizal/todoapp/datamodel/TodoData.java
@@ -1,6 +1,7 @@
 package com.mizal.todoapp.datamodel;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -10,13 +11,11 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
-import java.util.List;
 
 public class TodoData {
     private static TodoData instance = new TodoData();
     private static String filename = "TodoListItems.txt";
-
-    private List<TodoItem> todoItems;
+    private ObservableList<TodoItem> todoItems;
     private DateTimeFormatter formatter;
 
     public static TodoData getInstance() {
@@ -27,7 +26,7 @@ public class TodoData {
         formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     }
 
-    public List<TodoItem> getTodoItems() {
+    public ObservableList<TodoItem> getTodoItems() {
         return todoItems;
     }
 
@@ -80,5 +79,9 @@ public class TodoData {
                 bw.close();
             }
         }
+    }
+
+    public void deleteTodoItem(TodoItem item) {
+        todoItems.remove(item);
     }
 }
