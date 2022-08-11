5beb12c41192ee356bfe1d748b2cf4e21741f5fd
amirizal <amirizalrahmat@gmail.com>
2022-08-11 15:12:48 +0800

    comment unused function

--- src/main/java/com/mizal/todoapp/datamodel/TodoItem.java
+++ src/main/java/com/mizal/todoapp/datamodel/TodoItem.java
@@ -40,8 +40,8 @@ public class TodoItem {
         this.deadline = deadline;
     }
 
-    @Override
-    public String toString() {
-        return shortDescription;
-    }
+//    @Override
+//    public String toString() {
+//        return shortDescription;
+//    }
 }
