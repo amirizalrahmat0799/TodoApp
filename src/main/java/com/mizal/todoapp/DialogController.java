b4bc5c9e6a78cdfd46deb0bfd783ea7ecbbfdebd
amirizal <amirizalrahmat@gmail.com>
2022-08-11 13:15:45 +0800

    adding property and method to class. Method processResult are for dialogPane to insert new item into new dialog.fxml file

--- src/main/java/com/mizal/todoapp/DialogController.java
+++ src/main/java/com/mizal/todoapp/DialogController.java
@@ -1,4 +1,31 @@
 package com.mizal.todoapp;
 
+import com.mizal.todoapp.datamodel.TodoData;
+import com.mizal.todoapp.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import java.time.LocalDate;
+
 public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    @FXML
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription,details,deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
 }
+
