8bd21e830c485641001606dfe5c61246c2474511
amirizal <amirizalrahmat@gmail.com>
2022-08-11 13:17:17 +0800

    adding code to make listView updated everytime new data is insert into dialogPane

--- src/main/java/com/mizal/todoapp/Controller.java
+++ src/main/java/com/mizal/todoapp/Controller.java
@@ -6,13 +6,12 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 public class Controller {
 
@@ -48,15 +47,31 @@ public class Controller {
     public void showNewItemDialog() {
         Dialog<ButtonType> dialog = new Dialog<>();
         dialog.initOwner(mainBorderPane.getScene().getWindow());
+        dialog.setTitle("Add new todo item");
+        dialog.setHeaderText("Create new todo items");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("todoItemDialog.fxml"));
         try {
-            Parent root = FXMLLoader.load(getClass()
-                    .getResource("todoItemDialog.fxml"));
-            dialog.getDialogPane().setContent(root);
+
+            dialog.getDialogPane().setContent(fxmlLoader.load());
 
         } catch (IOException e) {
             System.out.println("Couldn't load the dialog");
             e.printStackTrace();
             return;
+        }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            DialogController controller = fxmlLoader.getController();
+            TodoItem newItem = controller.processResults();
+            todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+            todoListView.getSelectionModel().select(newItem);
+            System.out.println("Ok pressed");
+        } else {
+            System.out.println("Cancel pressed");
         }
 
     }
